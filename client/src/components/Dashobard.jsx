1910fa4d52ca9fd851d5a59b466b094f518f3ded
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-04-11 02:15:02 +0530

    Remove Layout

--- client/src/components/Dashobard.jsx
+++ client/src/components/Dashobard.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import {Layout} from 'antd'
 
-
-const {Sider , Header ,Content} = Layout;
 const Dashobard = () => {
   return (
-    <Layout>
-        <Sider theme='light' trigger={null} collapsible collapsed={collapsed}className='slider'></Sider>
-            <Layout>
-                <Header></Header>
-                <Content></Content>
-            </Layout>
-    </Layout>
+    <>
+      Dashboard
+    </>
   )
 }
 
