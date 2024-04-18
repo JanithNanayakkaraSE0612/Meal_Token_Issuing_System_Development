126089c04360b9658686c8f1275295edc6a72069
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-04-18 20:27:04 +0530

    Update list

---
+++ client/src/components/MealList.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+
+const MealList = () => {
+  return (
+
+    <Row gutter={16}>
+    <Col span={8}>
+    <Card
+    style={{
+      width: 300,
+    }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+    </Col>
+    <Col span={8}>
+    <Card
+    style={{
+      width: 300,
+    }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+    </Col>
+    <Col span={8}>
+    <Card
+    style={{
+      width: 300,
+    }}
+    cover={
+      <img
+        alt="example"
+        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+      />
+    }
+    actions={[
+      <SettingOutlined key="setting" />,
+      <EditOutlined key="edit" />,
+      <EllipsisOutlined key="ellipsis" />,
+    ]}
+  >
+    <Meta
+      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+    </Col>
+  </Row>
+   
+  )
+}
+
+export default MealList
