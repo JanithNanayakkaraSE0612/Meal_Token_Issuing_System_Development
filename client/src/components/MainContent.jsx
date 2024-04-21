3e8980036d39531a51cfdb3ecb821ab685baf39d
Pasindu Pabasara Pitigala <pasiya077@gmail.com>
2024-04-21 23:54:18 +0530

    dashboard nav bar

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import {Flex} from "antd";
-import { Col, Row } from 'antd';
 
+
+import React from 'react';
+import '../App.css';
 
 const MainContent = () => {
+    return (
+        <div className="wrapper" style={{ width: '150%'}}>
+            <div className="cards">
+                <div className="card [ is-collapsed ]">
+                    <div className="card__inner [ js-expander ]">
+                        <span>Item</span>
+                        <i className="fa fa-folder-o"></i>
+                    </div>
+                    <div className="card__expander">
+                        <i className="fa fa-close [ js-collapser ]"></i>
+                        Expander
+                    </div>
+                </div>
 
-    return(
-        <div className="wrapper" style={{width: '150%'}} >
-            <div className="cards"  >
-
-                <div className=" card [ is-collapsed ] " >
+                <div className="card [ is-collapsed ]">
                     <div className="card__inner [ js-expander ]">
                         <span>Card</span>
                         <i className="fa fa-folder-o"></i>
@@ -20,7 +29,7 @@ const MainContent = () => {
                     </div>
                 </div>
 
-                <div className=" card [ is-collapsed ] ">
+                <div className="card [ is-collapsed ]">
                     <div className="card__inner [ js-expander ]">
                         <span>Card</span>
                         <i className="fa fa-folder-o"></i>
@@ -30,22 +39,8 @@ const MainContent = () => {
                         Expander
                     </div>
                 </div>
-
-                <div className=" card [ is-collapsed ] ">
-                    <div className="card__inner [ js-expander ]">
-                        <span>Card</span>
-                        <i className="fa fa-folder-o"></i>
-                    </div>
-                    <div className="card__expander">
-                        <i className="fa fa-close [ js-collapser ]"></i>
-                        Expander
-                    </div>
-                </div>
-
             </div>
-
         </div>
-
     );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App.jsx";
 import './index.css'
+import MealList from "./components/MealList.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+     <MealList/>
   </React.StrictMode>,
 )
