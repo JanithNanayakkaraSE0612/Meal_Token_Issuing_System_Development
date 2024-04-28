cc731b63cfc66539c930c2854b8cd563f9a6b52a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-04-28 14:21:56 +0530

    Remove css some style

--- client/src/routes/AppRoute.jsx
+++ client/src/routes/AppRoute.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import ManageItems from '../components/ManageItems'
+import CompileMeals from '../components/CompileMeals'
 
 
 const AppRoute = () => {
   return (
     <BrowserRouter>
     <Routes>
-    {/*<Route path="/" element={<ManageItems/>}></Route>*/}
-    {/* <Route path="/inventory" element={<Inventory />}></Route>*/}
-    {/* <Route path="/orders" element={<Orders />}></Route>*/}
+    <Route path="/manageItems" element={<ManageItems/>}></Route>
+    <Route path="/compileMeals" element={<CompileMeals />}></Route>
+    <Route path="/" element={<Orders />}></Route>
     {/*<Route path="/customers" element={<Customers />}></Route>*/}
   </Routes>
   </BrowserRouter>
