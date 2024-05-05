03cdbcd762aa05ab505df7efe13e7d9a6934ab4a
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-05-05 13:13:29 +0530

    Updare

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //Routing
   <React.StrictMode>
     <BrowserRouter>
     <App />
