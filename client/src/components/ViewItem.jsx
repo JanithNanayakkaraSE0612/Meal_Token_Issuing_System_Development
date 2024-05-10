a7ba9b74a3f836dc8cd6ba1bd3bd04cc5a03bac5
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-05-10 16:48:42 +0530

    Update Routing Remove Component

--- client/src/components/ViewItem.jsx
+++ client/src/components/ViewItem.jsx
@@ -2,26 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Card, Row, Col } from "antd";
 import axios from "axios";
 
-const ViewItem = ({ itemId }) => {
-  const [item, setItem] = useState(null);
+const ViewItem = ({ items }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchItemData();
+    setLoading(false);
   }, []);
-
-  const fetchItemData = async () => {
-    try {
-      const response = await axios.get(`http://localhost:3001/items/${itemId}`);
-      setItem(response.data);
-    } catch (error) {
-      console.error("Error fetching item data:", error);
-      setError("Failed to fetch item data.");
-    } finally {
-      setLoading(false);
-    }
-  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -31,22 +18,23 @@ const ViewItem = ({ itemId }) => {
     return <p>Error: {error}</p>;
   }
 
-  if (!item) {
+  if (!items || items.length === 0) {
     return <p>No item data available.</p>;
   }
 
   return (
     <div style={{ padding: "24px" }}>
       <Row gutter={[16, 16]}>
-        <Col xs={24} sm={16} md={12} lg={8} xl={6}>
-          <Card
-            title={item.name}
-            cover={<img alt={item.name} src={item.image} />}
-          >
-            <p>Price: ${item.price}</p>
-            <p>Description: {item.description}</p>
-          </Card>
-        </Col>
+        {items.map((item) => (
+          <Col key={item.id} xs={24} sm={16} md={12} lg={8} xl={6}>
+            <Card
+              title={item.name}
+              cover={<img alt={item.name} src={item.image} />}
+            >
+              <p>Price: ${item.price}</p>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </div>
   );
--- client/src/routes/AppRoute.jsx
+++ client/src/routes/AppRoute.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ManageItems from "../components/ManageItems";
 import CompileMeals from "../components/CompileMeals";
-import ItemDetailsPage from "../components/ItemDetailsPage";
 import Dashboard from "../pages/Dashboard";
 import BrowseMeals from "../components/BrowseMeals";
 import ViewItem from "../components/ViewItem";
@@ -13,7 +12,6 @@ const AppRoute = () => {
         <Route path="/" element={<Dashboard />}></Route>
         <Route path="/manageItems" element={<ManageItems />}></Route>
         <Route path="/compileMeals" element={<CompileMeals />}></Route>
-        <Route path="/itemDetails" element={<ItemDetailsPage />}></Route>
         <Route path="/browseMeal" element={<BrowseMeals />}></Route>
         <Route path="/viewItem" element={<ViewItem />}></Route>
       </Routes>
