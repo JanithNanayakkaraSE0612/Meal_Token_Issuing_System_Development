49aaf446078a61b1acc27622677c99880d1cc03e
Pasindu Pabasara Pitigala <pasiya077@gmail.com>
2024-05-15 12:21:00 +0530

    responsive card tag

--- client/src/components/MealList.jsx
+++ client/src/components/MealList.jsx
@@ -1,36 +1,44 @@
 import React from "react";
-
-import { Card, Space } from "antd";
-import { Button, Dropdown, Flex } from "antd";
-
+import { Card, Space, Row, Col, Button } from "antd";
 
 const { Meta } = Card;
+
 const MealList = () => {
-  return (
-    <Space direction="vertical" size={2}>
-      <div style={{ width: 100 }}>
-        <div style={{ position: "absolute" }}>
-          <Card
-            style={{
-              width: 250,
-              marginLeft: 100,
-              top: 150,
-            }}
-            cover={<img alt="example" src="" />}
-            actions={[
-              <Button
-                type="added"
-                style={{ backgroundColor: "#F56A00", color: "white" }}
-              >
-                Added
-              </Button>,
-            ]}
-          >
-            <Meta title="Vegetable Fried Rice" />
-          </Card>
-        </div>
-      </div>
-    </Space>
-  );
+    const meals = [
+        {
+            title: "Vegetable Fried Rice",
+            img: "https://www.whiskaffair.com/wp-content/uploads/2018/11/Vegetable-Fried-Rice-2-3.jpg", // Add your image source here
+            id: 1,
+        },
+        // Add more meal items here
+    ];
+
+    return (
+        <Space direction="vertical" size={2} style={{ width: "100%", padding: "20px" }}>
+            <Row gutter={[16, 16]}>
+                {meals.map((meal) => (
+                    <Col xs={24} sm={12} md={8} lg={6} key={meal.id}>
+                        <Card
+                            style={{
+                                width: "100%",
+                            }}
+                            cover={<img alt={meal.title} src={meal.img} />}
+                            actions={[
+                                <Button
+                                    type="primary"
+                                    style={{ backgroundColor: "#F56A00", color: "white" }}
+                                >
+                                    Added
+                                </Button>,
+                            ]}
+                        >
+                            <Meta title={meal.title} />
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Space>
+    );
 };
+
 export default MealList;
