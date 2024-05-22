edc8653d2e43fff4f511d3accdb26e1ee81781e1
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-05-22 14:13:10 +0530

    Update Meal List  image prvevew Success

--- client/src/components/MealList.jsx
+++ client/src/components/MealList.jsx
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from "react";
 import { Card, Space, Row, Col, Button, message } from "antd";
 import axios from "axios";
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDFq8wtK0Cisnq5K8VNJIgJSkGsnV_PpSw",
+  authDomain: "image-upload-1c651.firebaseapp.com",
+  projectId: "image-upload-1c651",
+  storageBucket: "image-upload-1c651.appspot.com",
+  messagingSenderId: "201296211009",
+  appId: "1:201296211009:web:b798d3297c7748e7b92ac0",
+  measurementId: "G-YNY9SBYJVH",
+};
+
+initializeApp(firebaseConfig);
 const { Meta } = Card;
 
 const MealList = () => {
   const [items, setItems] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
+  const [imageUrls, setImageUrls] = useState({});
+  const [loadingImages, setLoadingImages] = useState({});
 
   useEffect(() => {
     fetchItems();
-    fetchCartItems();
   }, []);
 
   const fetchItems = async () => {
@@ -20,6 +33,7 @@ const MealList = () => {
 
       if (Array.isArray(responseData)) {
         setItems(responseData);
+        fetchImages(responseData);
       } else {
         console.error("Data received is not an array:", responseData);
       }
@@ -28,22 +42,28 @@ const MealList = () => {
     }
   };
 
-  const fetchCartItems = async () => {
-    try {
-      const response = await axios.get("https://eato.onrender.com/cart");
-      setCartItems(response.data.data || []);
-    } catch (error) {
-      console.error("Error fetching cart items:", error);
-    }
+  const fetchImages = async (items) => {
+    const storage = getStorage();
+    const imageUrlsPromises = items.map(async (item) => {
+      const storageRef = ref(storage, item.picture);
+      try {
+        const url = await getDownloadURL(storageRef);
+        return { [item.id]: url };
+      } catch (error) {
+        console.error("Error getting download URL:", error);
+        return { [item.id]: null };
+      }
+    });
+
+    const imageUrlsArray = await Promise.all(imageUrlsPromises);
+    const newImageUrls = imageUrlsArray.reduce((acc, curr) => ({ ...acc, ...curr }), {});
+    setImageUrls(newImageUrls);
   };
 
   const addToCart = async (meal) => {
     try {
-      const response = await axios.post("https://eato.onrender.com/cart", {
-        meal,
-      });
+      const response = await axios.post("https://eato.onrender.com/cart", { meal });
       if (response.status === 200) {
-        setCartItems((prevItems) => [...prevItems, meal]);
         message.success("Item added to cart!");
       } else {
         message.error("Failed to add item to cart.");
@@ -55,58 +75,54 @@ const MealList = () => {
   };
 
   return (
-      <div>
-        <Space
-            direction="vertical"
-            size={2}
-            style={{ width: "100%", padding: "20px" }}
-        >
-          <Row gutter={[16, 16]}>
-            {items.map((meal) => (
-                <Col xs={24} sm={12} md={8} lg={6} key={meal.id}>
-                  <Card
-                      style={{ width: "100%" }}
-                      title={meal.name}
-                      cover={
-                        <>
-                          <img
-                              alt={meal.name}
-                              src={meal.picture}
-                              style={{
-                                width: "100%",
-                                height: "200px",
-                                objectFit: "cover",
-                              }}
-                              onError={(e) => {
-                                e.target.onerror = null;
-                              }}
-                          />
-                        </>
-                      }
-                      actions={[
-                        <Button
-                            type="primary"
-                            style={{ backgroundColor: "#F56A00", color: "white" }}
-                            onClick={() => addToCart(meal)}
-                        >
-                          Add to Cart
-                        </Button>,
-                      ]}
-                  >
-                    <Meta
-                        title={meal.title}
-                        description={
-                          <div>
-                            <p style={{ margin: 0 }}>{meal.price}</p>
-                          </div>
-                        }
-                    />
-                  </Card>
-                </Col>
-            ))}
-          </Row>
-        </Space>
-      </div>
+    <div>
+      <Space direction="vertical" size={2} style={{ width: "100%", padding: "20px" }}>
+        <Row gutter={[16, 16]}>
+          {items.map((meal) => {
+            const imageUrl = imageUrls[meal.id];
+            const isLoading = loadingImages[meal.id] || false; // Check loading state
+
+            return (
+              <Col xs={24} sm={12} md={8} lg={6} key={meal.id}>
+                <Card
+                  style={{ width: "100%" }}
+                  title={meal.name}
+                  cover={
+                    isLoading ? (
+                      // Display loading indicator while image is being fetched
+                      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                        <div className="loader">Loading...</div>
+                      </div>
+                    ) : imageUrl ? (
+                      <img alt={meal.name} src={imageUrl} style={{ width: "100%", height: "200px", objectFit: "cover" }} />
+                    ) : (
+                      // Display placeholder image if image retrieval fails
+                      <img alt="Placeholder" src="path/to/placeholder.jpg" style={{ width: "100%", height: "200px", objectFit: "cover" }} />
+                    )
+                  }
+                  actions={[
+                    <Button type="primary" style={{ backgroundColor: "#F56A00", color: "white" }} onClick={() => addToCart(meal)}>
+                      Add to Cart
+                    </Button>,
+                  ]}
+                >
+                  <Meta
+                    title={meal.title}
+                    description={
+                      <div>
+                        <p style={{ margin: 0 }}>{meal.price}</p>
+                        {/* Display additional details if available in the API response */}
+                        {meal.description && <p>{meal.description}</p>}
+                      </div>
+                    }
+                  />
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Space>
+    </div>
   );
 };
 
